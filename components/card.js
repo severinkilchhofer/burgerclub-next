ca1991ef963f2e66d9e3d81dc7f37f7eec94c60b
Severin Kilchhofer <severin.kilchhofer@bluewin.ch>
2023-05-07 17:37:12 +0200

    fix logo height

--- components/card.js
+++ components/card.js
@@ -4,8 +4,8 @@ import {Text} from "./text";
 export const Card = ({restaurant, date}) => {
     return <Link href={`/${restaurant.id}`} className="hover:no-underline text-black group">
         <li className="shadow-card px-6 md:px-12 py-6 md:py-6 rounded-md mb-12 cursor-pointer md:transition md:duration-500 md:ease-in-out md:transform md:hover:-translate-y-1 md:hover:scale-105">
-            <div className="flex content-between items-baseline">
-                {restaurant.icon ? <img src={restaurant.icon.file.url} alt="Restaurant Logo" className="w-6/12 md:w-3/12"/>
+            <div className="flex content-between pb-2">
+                {restaurant.icon ? <div className="flex justify-items-start w-6/12 md:w-3/12"><img src={restaurant.icon.file.url} alt="Restaurant Logo" className="h-16 sm:h-24 object-contain"/></div>
                     : <h3 className="w-full"><Text text={restaurant.properties.Restaurant.title}/></h3>
                 }
                 <h1 className="w-full text-right">{restaurant.properties.Burger_Rating ? restaurant.properties.Burger_Rating.number : ''}</h1>
